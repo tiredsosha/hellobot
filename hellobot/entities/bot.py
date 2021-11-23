2103ee5e68644360a33dd850f391afdb887b9736
Чичко Александра Алексеевна <aachichko@sberautotech.ru>
2021-11-24 01:53:58 +0300

    no more creds

--- hellobot/entities/bot.py
+++ hellobot/entities/bot.py
@@ -2,6 +2,6 @@ from pydantic import BaseModel
 
 
 class Bot(BaseModel):
-    singing: str = '5672df6abcdcdfbbdcbdfea0ab52135e'
-    token: str = 'xoxb-1572232124883-2138932977143-kSVbvCD3zzFFDdA1CUMpaT25'
+    singing: str = 'xxxxx'
+    token: str = 'xoxb-xxxx'
     endpoint: str = '/slack/events'
